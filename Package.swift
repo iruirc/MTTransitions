cb6c954233fd1eb7f7b4a274d636d905a60b0657
Dmitry <Iruirc@gmail.com>
2025-09-26 13:39:12 +0300

    Dependency 'MetalPetal': import as dynamic library

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "MTTransitions",
-                dependencies: ["MetalPetal"],
+                dependencies: [
+                    .product(name: "MetalPetal-Dynamic", package: "MetalPetal"),
+                ],
                 path: "Source")
     ],
     swiftLanguageVersions: [.v5]
